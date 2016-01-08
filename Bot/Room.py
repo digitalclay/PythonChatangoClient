4e2f2aad3053be6386d08cd516587c2ef09994e6
digitalclay <jprollins5787@yahoo.com>
2016-01-08 16:09:45 -0500

    Update Room.py

--- Bot/Room.py
+++ Bot/Room.py
@@ -1,3 +1,15 @@
+###############################################
+#Room Bot - Jon Rollins - J.p. - Digitalclay ####
+#This code and all code posted is in no way ###
+#to be takenn as serious. Thrown together #####
+#in about an hour as an example of how  #######
+#simple it can be to write a bot from scratch,#
+#without using exsiting libraries.This code ###
+#purposly didnt use classes ect. to keep code##
+#to a bare minimum. ###########################
+###############################################
+# mizukebot #
+
 ###################
 ## Imports ########
 
@@ -12,13 +24,14 @@ import socket
 from threading import Timer
 from threading import Thread
 import random
+import re
 
 ##################
 ## Variables #####
 
 uid = ''
-username = 'Mizukebot'
-password = ''
+username = 'mizukebot'
+password = 'password-_____________-'
 host = ''
 port = 5228;
 roomName = "digitalmasterminds69"
@@ -54,10 +67,13 @@ fontSize = "14";
 fontColor = "000";
 nameColor = "093";
 
+
+
 ####################
 ## Connect To Pms ##
 
 def connect():
+
 
     uid = genUID()
     print(uid)
@@ -127,90 +143,98 @@ def run():
   
     global running
     running = True
-
+    
+    global quiet
+    quiet = False
+    
     while running:
-
+      
+        if(writeLock == True):
+          print("[Room] Error : Can't read while writing to server!!! ")
+          continue
+        
+        if(quiet == True):
+          print("[Room] Quiet "+quiet)
+          continue
+        
         msg = str(sock.recv(1024).decode("utf-8"))
         
         try:
             if msg.startswith("b:"):
-              onMsgRcv(msg)
-              #print("[Room] Input : "+msg)
+              onMsgRcv(cleanMsg(msg))
+
             if msg.startswith("ok:"):
-              onConnectRcv()
-              print("[Room] Input : "+msg)
+              onConnectRcv(msg)
               
             if msg.startswith("denied"):
               onConnectFailRcv()
-              print("[Room] Input : "+msg)
               
-            if msg.startswith("inited"):
-              onInitRCV(msg)
-              print("[Room] Input : "+msg)
+            if "inited" in msg:
+              onInitRcv(msg)
               
             if msg.startswith("n:"):
               onCountRcv(msg)
-              print("[Room] Input : "+msg)
               
             if msg.startswith("mods:"):
               onModsRcv(msg)
-              print("[Room] Input : "+msg)
               
             if msg.startswith("participant:0"):
               onLeaveRcv(msg)
-              print("[Room] Input : "+msg)
               
             if msg.startswith("participant:1"):
               onJoinRcv(msg)
-              print("[Room] Input : "+msg)
               
             if msg.startswith("g_participants:"):
               onRoomNamesRcv(msg)
-              print("[Room] Input : "+msg)
               
             if msg.startswith("blocked:"):
               onBanRcv(msg)
-              print("[Room] Input : "+msg)
               
             if msg.startswith("unblocked:"):
               onUnbanRcv(msg)
-              print("[Room] Input : "+msg)
 
             if msg.startswith("i:"):
               onPastMsgRcv(msg)
-              print("[Room] Input : "+msg)
 
             if msg.startswith("blocklist:"):
               onBlocklistRcv(msg)
-              print("[Room] Input : "+msg)
 
             if msg.startswith("delete:"):
               onMsgDeleteRcv(msg)
-              print("[Room] Input : "+msg)
 
             if msg.startswith("show_fw:"):
               onFloodWarningRcv(msg)
-              print("[Room] Input : "+msg)
 
             if msg.startswith("show_tb:"):
               onFloodBanRcv(msg)
-              print("[Room] Input : "+msg)
 
             if msg.startswith("tb:"):
               onFloodBanRepeatRcv(msg)
-              print("[Room] Input : "+msg)
 
             if msg.startswith("premium:"):
               onPremiumRcv(msg)
-              print("[Room] Input : "+msg)
 
             if msg.startswith("deleteall:"):
               onAllMsgDeleteRcv(msg)
-              print("[Room] Input : "+msg)
-              
+
         except Exception:
             print(str(sys.exc_info()))
             continue
+
+
+
+
+############################
+#Clean message from server##
+def cleanMsg(msg):
+  msg = re.sub("<.*?>", "", msg)
+  msg = re.sub("â–“", "", msg)
+  msg = re.sub("â–’", "", msg) 
+  msg = msg.replace("&lt;", "<").replace("&gt;", ">").replace("&quot;", "\"").replace("&apos;", "'").replace("&amp;", "&")
+  return msg
+
+
+
 
 
 ##############################
@@ -224,66 +248,177 @@ def onMsgRcv(msg):
   print("[Room] MSG ("+sender+") : "+message)
   checkCommands(sender,message)
 
+
 #On Connect
-def onConnectRcv():
+def onConnectRcv(msg):
+  if ":" in msg:
+    global owner
+    owner = msg.split(":")[1]
+    print("[Room] Owner : "+owner)
+  else:
+    return
+  if "M:" not in msg:
+    onLoginFail()
+    return
+  global mods
+  mods = []
+  msgArray = msg.split("M:")  
+  msgArray2 = msgArray[1].split(":")
+  msgArray3 = msgArray2[3].split(";")
+  m = 0
+  for mod in msgArray3:
+    mods.append(msgArray3[m].split(",")[0])
+    m += 1 
+  print("[Room] Mods : "+str(mods))
   sendMsg("Login Successful ^.^")
+
+
+#login fail
+def onLoginFail():
+  print("[Room] Error : Failed to login")
 
 
 #Connect Fail
 def onConnectFailRcv():
+  print("[Room] Error : Connnect Failled : "+msg)
   running = False
   sock.close()
   sock = None
 
+
+#init
 def onInitRcv(msg):
-  pass
+  getPremium()
+  getRoomUsers()
+  getBlockList()
 
+#Room count
 def onCountRcv(msg):
-  pass
+  global count
+  count = msg.split("n:")[1]
+  print("[Room] Count : "+str(count))
 
+#mod add or remove
 def onModsRcv(msg):
-  pass
+  msg = msg.replace("mods:", "")
+  if ":" not in msg:
+    return
+  msgParts = msg.split(":")
+  newMods = []
+  m = 0
+  for mod in msgParts:
+    newMods.append(msgParts[m].split(",")[0])
+    m += 1
+  global mods
+  if len(mods) > len(newMods):
+    mods = newMods
+    print("[Room] Mod removed :"+str(mods))
+  if len(mods) < len(newMods):
+    mods = newMods
+    print("[Room] Mod Added :"+str(mods))
 
+
+#user leaves chat
 def onLeaveRcv(msg):
-  pass
+  user = msg.split(":")[4]
+  global users
+  users.append(user)
+  print("[Room] User left Chat : "+user)
 
+
+#user joins chat
 def onJoinRcv(msg):
-  pass
+  user = msg.split(":")[4]
+  global users
+  users.append(user)
+  print("[Room] User Joined Chat : "+user)
 
+
+#on user names rcv
 def onRoomNamesRcv(msg):
-  pass
+  msg = msg.replace("g_participants:","")
+  parts = msg.split(";")
+  u = 0
+  global users
+  users = []
+  for user in parts:
+    users.append(parts[u].split(":")[3])
+    u += 1
+  print("[Room] Users : "+str(users))
 
+
+#blocklist 
+def onBlocklistRcv(msg):
+  msg = msg.replace("blocklist:","")
+  global banList
+  banList = []
+  if ";" not in msg and ":" not in msg:
+    print("[Room] BanList : "+str(banList))
+    return
+  parts = msg.split(";")
+  b = 0
+  for ban in parts:
+    banList.append(parts[b].split(":")[2])
+    b += 1
+  print("[Room] BanList : "+str(banList))
+
+
+#on ban
 def onBanRcv(msg):
-  pass
+  banned = msg.split(":")[3]
+  banner = msg.split(":")[4]
+  banList.append(banned)
+  print("[Room] Ban : "+banned+" By mod : "+banner)
 
-
+#on unban
 def onUnbanRcv(msg):
-  pass
+  banned = msg.split(":")[3]
+  banner = msg.split(":")[4]
+  banList.remove(banned)
+  print("[Room] UnBan : "+banned+" By mod : "+banner)
 
-
+#past msgs **unused**
 def onPastMsgRcv(msg):
   pass
 
-def onBlocklistRcv(msg):
-  pass
-
+#msg deleted
 def onMsgDeleteRcv(msg):
-  pass
+  print("[Room] Messages deleted"+msg)
+  
 
+#flood warning
 def onFloodWarningRcv(msg):
-  pass
-
+  print("[Room] Flood Warning!!!")
+  if quiet == False:
+    setQuiet(True)
+    Timer(30.0, setQuiet(False)).start()
+  
+#flood Ban
 def onFloodBanRcv(msg):
-  pass
+  print("[Room] Flood Banned!!!")
 
+#flood ban repeat
 def onFloodBanRepeatRcv(msg):
-  pass
+  print("[Room] Flood Ban Repeat!!!")
 
+#set quiet
+def setQuiet(mode):
+  global quiet
+  quiet = mode
+
+#on premium rcv
 def onPremiumRcv(msg):
-  pass
+  if "200" in msg:
+    print("[Room] Premium available")
+    setBG()
+    setMedia()
+  if "111" in msg:
+    print("[Room] Premium not available")
+    return
 
+#on all msg deleted
 def onAllMsgDeleteRcv(msg):
-  pass
+  print("[Room] All messages deleted")
 
 
 
@@ -292,9 +427,20 @@ def onAllMsgDeleteRcv(msg):
 
 #Send
 def sendToServer(message , usePrint):
+  global writeLock
+  writeLock = True
   if(usePrint):
     print("[Room] Output : "+str(message.encode()))
-  sock.send(str(message).encode())
+  try:
+    checkDone = sock.sendall(str(message).encode())
+    while(checkDone != None):
+      continue
+    writeLock = False
+  except Exception:
+    print("error in write")
+    writeLock = False
+    return
+
 
 #Login
 def sendLogin():
@@ -310,18 +456,91 @@ def ping():
   sendToServer("\r\n", False)
   Timer(20.0, ping).start()
 
+#get room users
+def getRoomUsers():
+  sendToServer("g_participants:start\r\n", True)
 
+#get premium
+def getPremium():
+  sendToServer("getpremium:1\r\n", True)
 
+# get bans
+def getBlockList():
+  sendToServer("blocklist:block::next:500\r\n", True)
 
+#set bg
+def setBG():
+  sendToServer("msgbg:1\r\n",True)
 
+#set video/audio recording
+def setMedia():
+  sendToServer("msgmedia:1\r\n",True)
+
+#add mod
+def addMod(name):
+  sendToServer("addmod:"+name+"\r\n")
+
+#remove mod
+def removeMod(name):
+  sendToServer("removemod:"+name+"\r\n")
+
+#ban
+def ban(userid,ip,name):
+  sendToServer("block:"+userID+":"+ip+":"+name+"\r\n")
+
+#unban  
+def unBan(userid,ip,name):
+  sendToServer("removeblock:"+userID+":"+ip+":"+name+"\r\n")
+
+#delete msg
+def deleteMsg(msgID):
+  sendToServer("delmsg:"+msgID+"\r\n")
+
+#delete all user msg  
+def deleteUserAllMsg(userID):
+  sendToServer("delallmsg:"+userID+"\r\n")
+
+#flag
+def flag(msgID):
+  sendToServer("g_flag:"+msgID+"\r\n")
+
+#clear chat
+def clearAllMsg(name):
+  sendToServer("clearall:\r\n")
 
 
 ############################
 ## Check Bot Commands ######
 def checkCommands(sender,message):
-  pass
+  
+  #example commannds
+  if message.lower().startswith("-cmd"):
+    sendMsg("My Example commands are  -mods , -count , -users , -bans , -quiet , -addmod , -removemod")
+  
+  if message.lower().startswith("-mods"):
+    sendMsg("Owner - "+ owner + " Mods - " + str(mods))
 
+  if message.lower().startswith("-count"):
+    sendMsg("Room Count - "+str(count))
 
+  if message.lower().startswith("-users"):
+    sendMsg("Users - "+str(users))
+    
+  if message.lower().startswith("-bans"):
+    sendMsg("Ban List - "+str(banList))
+    
+  if message.lower().startswith("-quiet"):
+    if quiet == True :
+      setQuiet(False)
+    else:
+      setQuiet(True)
+    sendMsg("Quiet - "+quiet)
+
+  if message.lower().startswith("-addmod") and username == owner:
+    addMod(msg.split(" ")[1])
+    
+  if message.lower().startswith("-removemod") and username == owner:
+    removeMod(msg.split(" ")[1])
 
 #############
 ## Connect ##
